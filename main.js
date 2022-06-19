1cac43c988f0e67b003baa052da3c63ce4fda916
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-06-19 09:38:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,4 +15,25 @@ recognition.onresult=function(event)
     console.log(content);
 
     document.getElementById("textbox").innerHTML=content;
+
+    speak();
 }
+
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+
+    var utter=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter);
+
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
