5ff7a5e4e5158e90b58daf71ba94a83b9d02e77c
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-06-26 09:40:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,18 +16,31 @@ recognition.onresult=function(event)
 
     document.getElementById("textbox").innerHTML=content;
 
-    speak();
+    if(content=="take my selfie")
+    {
+        console.log("taking selfie----");
+
+        speak();
+    }
+
 }
 
 function speak()
 {
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="taking your selfie in 5 seconds";
 
     var utter=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utter);
 
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+take_snapshot();
+save();
+    },5000);
+
 }
 
 Webcam.set({
@@ -37,3 +50,19 @@ Webcam.set({
     png_quality:90
 });
 camera=document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+
+    });
+}
+
+function save()
+{
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
